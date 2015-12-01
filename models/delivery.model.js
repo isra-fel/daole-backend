7bd29e7fd9bf7e050ddaeabd924842e6dc78173b
Yeming Liu <liuym13@lenovo.com>
2015-12-01 17:47:12 +0800

    Enrich delivery model

--- models/delivery.model.js
+++ models/delivery.model.js
@@ -2,11 +2,33 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var delivery = new Schema({
-	company: String,
-	deliveryId: String,
-	username: {type: String, ref: 'User'},
-	isPinned: Boolean,
-	isReceived: Boolean
+	company: {
+		type: String,
+		required: 'Company is required',
+		trim: true
+	},
+	deliveryId: {
+		type: String,
+		required: 'Delivery ID is required',
+		trim: true
+	},
+	alias: {
+		type: String,
+		trim: true
+	},
+	username: {
+		type: String,
+		ref: 'User',
+		required: 'username is required'
+	},
+	isPinned: {
+		type: Boolean,
+		required: 'isPinned is required'
+	},
+	isReceived: {
+		type: Boolean,
+		required: 'isPinned is required'
+	}
 });
 
 mongoose.model('Delivery', delivery);
